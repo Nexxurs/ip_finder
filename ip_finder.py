202c713b9ee7c1874a20ee4acbd5ea8cf660f8b6
Nexxurs <p.uebermasser@gmx.at>
2018-03-13 11:28:29 +0100

    rewrote into methods

--- ip_finder.py
+++ ip_finder.py
@@ -1,22 +1,41 @@
 #!/usr/bin/env python3
 
-import socket
 
-IP = "<broadcast>"
-PORT = 17155
-MESSAGE = "getPI"
+class Pi:
+    def __init__(self, ip, hostname):
+        self.ip = ip
+        self.hostname = hostname
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-sock.sendto(MESSAGE.encode(), (IP, PORT))
-sock.settimeout(1)
+    def __str__(self):
+        return "{}: {}".format(self.hostname, self.ip)
 
-try:
-    while True:
-        data, (ip, port) = sock.recvfrom(1024)
-        print("%s: %s" % (data.decode(), ip))
-except socket.timeout:
-    pass
-finally:
-    sock.close()
-input("Press Enter to Close")
+
+def find_pis():
+    import socket
+
+    IP = "<broadcast>"
+    PORT = 17155
+    MESSAGE = "getPI"
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+    sock.sendto(MESSAGE.encode(), (IP, PORT))
+    sock.settimeout(1)
+
+    pis = []
+
+    try:
+        while True:
+            data, (ip, port) = sock.recvfrom(1024)
+            pi = Pi(ip, data.decode())
+            pis.append(pi)
+    except socket.timeout:
+        pass
+    finally:
+        sock.close()
+        return pis
+
+
+if __name__ == '__main__':
+    print(*find_pis(), sep='\n')
+    input("Press Enter to Close")
