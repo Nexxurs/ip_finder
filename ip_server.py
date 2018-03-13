ebeffb06ea6073b8ec7b71f6355bf389c59dcfb3
Nexxurs <p.uebermasser@gmx.at>
2018-03-13 11:18:41 +0100

    reformatted file
    tabs and spaces

--- ip_server.py
+++ ip_server.py
@@ -7,11 +7,11 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 sock.settimeout(None)
 try:
-	while True:
-		data, addr = sock.recvfrom(1024)
-		txt = data.decode()
-		if txt == "getPI":
-			host = socket.gethostname()
-			sock.sendto(host.encode(), addr)
+    while True:
+        data, addr = sock.recvfrom(1024)
+        txt = data.decode()
+        if txt == "getPI":
+            host = socket.gethostname()
+            sock.sendto(host.encode(), addr)
 finally:
-	sock.close()
+    sock.close()
